376b5a97bf0854e7e295604eaf2f01042dda2328
trishita02 <trishu.kesarwani06@gmail.com>
2024-01-01 00:21:10 -0800

    1st Jan POTD done

---
+++ second_largest.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    set<int> temp;
+	    for(int i=0;i<n;i++)
+	    temp.insert(arr[i]);
+	    if(temp.size()==1) return -1;
+	    auto itr=temp.end();
+	    itr--; itr--;
+	    return *itr;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.print2largest(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
